25283ff7d5b9239442eda1da19c5d0fc86fc8803
dustyooyu <somecalm@gmail.com>
2023-04-25 08:31:29 +0900

    first commit

---
+++ src/main/java/TestA.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class TestA {
+    public static void main(String[] args){
+
+   Scanner sc = new Scanner(System.in);
+   System.out.printf("%d" , sc.nextInt()+sc.nextInt());
+    }
+
+}
