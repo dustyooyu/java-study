4c71ff9bb6269c9ab357fb3c148cbb3260ded07c
dustyooyu <somecalm@gmail.com>
2023-05-01 10:57:46 +0900

    CodeUp 1070

---
+++ src/main/java/org/example/CodeUp/num10/CodeUp1070For.java
@@ -0,0 +1,18 @@
+package org.example.CodeUp.num10;
+
+import java.util.Scanner;
+
+public class CodeUp1070For {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        for (int i = 0; input != 0; i++) { // input이 0이면 반복 종료
+            System.out.println(input);
+            input = sc.nextInt();
+        }
+    }
+}
+
+// -2147483648 ~ +2147483647 범위 정수가 순서대로 입력된다.
+// 0이 아니면 입력된 정수를 줄을 바꿔 출력, 0이 입력되면 출력 중단.
+
---
+++ src/main/java/org/example/CodeUp/num10/CodeUp1070While.java
@@ -0,0 +1,18 @@
+package org.example.CodeUp.num10;
+
+import java.util.Scanner;
+
+public class CodeUp1070While {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) { // 무한반복문
+            int iNum = sc.nextInt();
+            if (iNum == 0) { // 입력받은 값이 0일시 break문 실행
+                break;
+            }
+            System.out.println(iNum); // 입력받은 값 출력 반복
+        }
+    }
+}
+// -2147483648 ~ +2147483647 범위 정수가 순서대로 입력된다.
+// 0이 아니면 입력된 정수를 줄을 바꿔 출력, 0이 입력되면 출력 중단.
