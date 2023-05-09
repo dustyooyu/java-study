9be9b02ee252436b83d84907293d94a852265e52
dustyooyu <somecalm@gmail.com>
2023-05-09 20:58:00 +0900

    Revert "Week4 day1"
    This reverts commit f390edb3749d6505daada214b4cdd4247c500e45.

--- src/main/java/org/example/Week4/Day15_230508/UserFactoryTest.java
+++
@@ -1,9 +0,0 @@
-package org.example.Week4.Day15_230508;
-
-public class UserFactoryTest {
-    public static void main(String[] args) {
-        UserFactory userFactory = new UserFactory(); // 인스턴스 생성
-        User user = userFactory.getAdultUser(); // 메소드접근, 리턴값을 참조타입 User 변수 user에 할당
-        System.out.println(user.age);
-    }
-}
