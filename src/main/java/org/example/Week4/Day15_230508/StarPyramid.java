8525cff4308176b73e7d514343816270bc89771d
dustyooyu <somecalm@gmail.com>
2023-05-12 15:40:28 +0900

    주석 수정

--- src/main/java/org/example/Week4/Day15_230508/StarPyramid.java
+++ src/main/java/org/example/Week4/Day15_230508/StarPyramid.java
@@ -8,7 +8,7 @@ public class StarPyramid {
         int height = 5;
         // n단 피라미드
         for (int i = 1; i <= height; i++) { // i는 1부터 층수될 때까지 +1며 반복
-            for (int j = 0; j < height - i; j++) { // i일 때 왼쪽공백은 0부터 높이-1보다 작을 때까지 공백반복출력
+            for (int j = 0; j < height - i; j++) { // i일 때 왼쪽공백은 0부터 높이-i보다 작을 때까지 공백반복출력
                 System.out.printf(" ");            // j가 증가할 수록 반복출력 횟수는 줄어든다.
             }
             for (int j = 2; j < i * 2 + 1; j++) { // i일 때 *은 홀수개로 증가. 2 * i + 1씩. 반복횟수 증가.
