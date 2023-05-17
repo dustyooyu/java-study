4e038a32c9b56e6ca24b117bd8d235373efc17b5
dustyooyu <somecalm@gmail.com>
2023-05-18 08:33:41 +0900

    출력테스트용 구분선 등 추가

--- src/main/java/org/example/Week5/Day20_230515/BubbleEx.java
+++ src/main/java/org/example/Week5/Day20_230515/BubbleEx.java
@@ -10,21 +10,23 @@ public class BubbleEx {
                 arr[i] = arr[i + 1];
                 arr[i + 1] = tmp;
             }
+            System.out.println("i "+i +Arrays.toString(arr));
         }
+        System.out.println("----------------------");
         return arr;
     }
 
     public int[] sort(int[] arr){
         for (int j = 1; j <= arr.length; j++) {
             arr = sortAround(arr, arr.length - j);
-            //System.out.println(Arrays.toString(arr));
-        }
+            System.out.println("j "+j +Arrays.toString(arr));
+            System.out.println("----------------------");        }
         return arr;
     }
 
     public static void main(String[] args) {
         BubbleEx bubble = new BubbleEx();
-        int[] arr = new int[]{7, 2, 3, 9, 28, 1};
+        int[] arr = new int[]{7, 2, 3, 9, 28, 4};
         System.out.println("정렬 전 " + Arrays.toString(arr));
         arr = bubble.sort(arr);
         System.out.println("정렬 후 " + Arrays.toString(arr));
