430fbff10e09b566e77ae5eb84d15c4879b29cd3
dustyooyu <somecalm@gmail.com>
2023-05-30 16:26:27 +0900

    feat: 윤년 print 기능 추가

--- src/main/java/org/example/Week7/Day29/LeapYear.java
+++ src/main/java/org/example/Week7/Day29/LeapYear.java
@@ -3,13 +3,29 @@ package org.example.Week7.Day29;
 public class LeapYear {
     public static void main(String[] args) {
         int year = 2020;
-        checkLeafYear(year);
+        printcheckLeapYear(year);
     }
 
-    private static void checkLeafYear(int year) {
-        if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+    private static void printcheckLeapYear(int year) {
+        if (checkLeapYear(year))
             System.out.println(year + " O");
         else
             System.out.println(year + " X");
     }
+
+    public static boolean checkLeapYear(int year) {
+        return ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0));
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) return true;
+        if (year % 100 == 0) return false;
+        if (year % 4 == 0) return true;
+        else {
+            return false;
+        }
+    }
+
+
 }
+
