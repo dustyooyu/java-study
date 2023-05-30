b299d0e84b0f1c1727e4f7b10fe1490e8572b115
dustyooyu <somecalm@gmail.com>
2023-05-30 11:08:34 +0900

    윤년 구하기 메서드 분리

--- src/main/java/org/example/Week7/Day29/LeapYear.java
+++ src/main/java/org/example/Week7/Day29/LeapYear.java
@@ -3,6 +3,10 @@ package org.example.Week7.Day29;
 public class LeapYear {
     public static void main(String[] args) {
         int year = 2020;
+        checkLeafYear(year);
+    }
+
+    private static void checkLeafYear(int year) {
         if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
             System.out.println(year + " O");
         else
