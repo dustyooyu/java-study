0e367737e427c52c91164a15d94e447b70991e64
dustyooyu <somecalm@gmail.com>
2023-05-31 14:23:46 +0900

    Person 전체조회

--- src/main/java/org/example/DB/DBMain.java
+++ src/main/java/org/example/DB/DBMain.java
@@ -1,41 +1,81 @@
 package org.example.DB;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class DBMain extends BaseDAO{
-
+public class DBMain extends BaseDAO {
     public static void main(String[] args) {
         DBMain dbMain = new DBMain();
-        dbMain.getCodeName();
+//        dbMain.getCodeName();
         dbMain.initPerson();
+        System.out.println(dbMain.insertPerson("leo"));
+        System.out.println(dbMain.insertPerson("yui"));
+        List<Person> personList = dbMain.findAllPerson();
+        System.out.println(personList.toString());
+    }
+
+    private List<Person> findAllPerson(){
+        List<Person> result = new ArrayList<>();
+        String sql = "select id, name from person";
+        try {
+            getConn();
+            psmt = conn.prepareStatement(sql);
+            rs = psmt.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt(("id"));
+                String name = rs.getString(("name"));
+                result.add(new Person(id, name));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally { // 자원이 존재한다면 전부 해제할 것
+            close();
+        }
+        return result;
+    }
+    private int insertPerson(String name) {
+        int cnt = 0;
+        String sql = "insert into person(name) values(?)";
+        try {
+            getConn();
+            psmt = conn.prepareStatement(sql);
+            psmt.setString(1, name);
+            cnt = psmt.executeUpdate();
+        }
+        catch(SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        finally {
+            close();
+        }
+        return cnt;
     }
 
     private void initPerson() {
-
+        String sql = """
+                create table person (
+                id integer primary key autoincrement,
+                name string
+                )
+                """;
         try {
             getConn();
             smt = conn.createStatement();
-            smt.setQueryTimeout(30);  // set timeout to 30 sec.
-
             smt.executeUpdate("drop table if exists person");
-            smt.executeUpdate("create table person (id integer, name string)");
-            smt.executeUpdate("insert into person values(1, 'leo')");
-            smt.executeUpdate("insert into person values(2, 'yui')");
-            rs = smt.executeQuery("select * from person");
-            while (rs.next()) {
-                // read the result set
-                System.out.println("name = " + rs.getString("name"));
-                System.out.println("id = " + rs.getInt("id"));
-            }
-        } catch (SQLException e) {
+            smt.executeUpdate(sql);
+        }
+        catch(SQLException e) {
             System.err.println(e.getMessage());
-        } finally {
-           close();
+        }
+        finally {
+            close();
         }
     }
 
+
     private void getCodeName() {
-        String sql = "select code, name from country order by code, name";
+        String sql = "select code, name from country";
 
         try {
             getConn();
