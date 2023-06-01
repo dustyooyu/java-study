c179de803838101267af97f90209db2961cc7fc5
dustyooyu <somecalm@gmail.com>
2023-06-02 00:02:36 +0900

    Member DAO 및 Test

---
+++ src/main/java/org/example/opgame/model/Member.java
@@ -0,0 +1,19 @@
+package org.example.opgame.model;
+
+public class Member {
+    String id;
+    boolean isLogined;
+
+    public Member(String id, boolean isLogined) {
+        this.id = id;
+        this.isLogined = isLogined;
+    }
+
+    public String getID() {
+        return id;
+    }
+
+    public boolean isLogined() {
+        return isLogined;
+    }
+}
